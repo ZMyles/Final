b91e9567dd63d5269fd87ce02ed9c527a9540d15
Zavier <zmyles@comcast.net>
2018-09-19 14:33:03 -0400

    Testing

--- PreGame/js/components/victory.js
+++ PreGame/js/components/victory.js
@@ -3,34 +3,33 @@
 const victory = {
   template: `
   <div class="movingBackground">
-   <h1 class="winner">You Win!</h1>
+    <h1 class="winner">You Win!</h1>
 
 
-  //  <div class="move">
+    <div class="move">
 
-   <div class="flip-container" ontouchstart="this.classList.toggle('hover')">
-                <div class="flipper">
-                    <div class="front">
-              <p><p>{{$ctrl.randomDranks.name}}<p></p>
-              
-                    </div>
-                  <div class="back"> 
-                  <h2>Ingredients</h2>
-                  <p class="ingreed">{{$ctrl.randomDranks.ingredient}}</p>
-              
-                    </div>
-             </div>
-   
-             
-   </div>
-</div>
-    
-<li><a href="#!/trivia">Try Again?</a></li>
-   </div>
-</div>
-           
+      <div class="flip-container" ontouchstart="this.classList.toggle('hover')">
+        <div class="flipper">
+            <div class="front">
+              <p>{{$ctrl.randomDranks.name}}</p>
+              <img ng-src="{{$ctrl.randomDranks.image}}"/>
+            </div>
+            <div class="back"> 
+              <h2>Ingredients</h2>
+              <p class="ingreed">{{$ctrl.randomDranks.ingredient}}</p>   
+            </div>         
+        </div> 
+      </div> 
+      <nav
+        <ul>
+            <li class="tryAgain"><a href="#!/home">try again</a></li>
+        </ul>
+      </nav>
+
+    </div>
+  </div>
+
  
-
   
   `,
   controller:["Quiz", "$location" ,function(Quiz,$location){
